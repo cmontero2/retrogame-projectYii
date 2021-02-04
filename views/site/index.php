fb845a93a9e85c8a5a8e98f806f10bb93a232b75
Cristina Montero <cmontero@iesfuentesanluis.org>
2021-02-04 21:03:02 +0100

    cambio pantalla principal

--- views/site/index.php
+++ views/site/index.php
@@ -7,10 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Bienvenido!</h1>
+        <h1 style="text-align: center">Bienvenido a </h1>
 
-        <p class="lead">Estás en el backend de: Retrogame</p>
-        <img src="img/logo-Retrogame.png"></img>
+        <img style="display: block; margin: auto;" src="img/logo-Retrogame.png"></img>
         
     </div>
 
@@ -19,7 +18,7 @@ $this->title = 'My Yii Application';
         <div class="row">
             <div class="col-md-12" style="text-align:center">
                 
-            <p><a class="btn btn-lg btn-primary" href="http://alum1.iesfsl.org:4200/">Ir al frontend</a></p>
+            <p><a class="btn btn-primary" href="http://alum1.iesfsl.org:4200/">Ir al frontend</a></p>
             
             </div>
         </div>
