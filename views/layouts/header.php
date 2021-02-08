64f6a68283f0f9a665c51f490279ac15940609ac
cmontero2 <cmontero@iesfuentesanluis.org>
2021-02-08 10:34:54 +0100

    cambio diseño y solucion doble login

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b><?=$title?></b></span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b><?=$title?></b> Administrator</span>
+          <span class="logo-lg"><b><?=$title?></b></span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
@@ -256,44 +256,6 @@ use yii\helpers\Html;
                 }
               ?>
 
-              <!-- User Account: style can be found in dropdown.less -->
-              <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-					<?= Html::img('@web/img/profileuser.jpg', ['class' => 'user-image', 'alt'=>'User Image']) ?>
-                  <span class="hidden-xs">Admin</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <!-- User image -->
-                  <li class="user-header">
-                    <?= Html::img('@web/img/profileuser.jpg', ['class' => 'img-circle', 'alt'=>'User Image']) ?>
-                    <p>
-                      Admin - Web Developer
-                      <small>Member since Nov. 2012</small>
-                    </p>
-                  </li>
-                  <!-- Menu Body -->
-                  <li class="user-body">
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Followers</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Sales</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Friends</a>
-                    </div>
-                  </li>
-                  <!-- Menu Footer-->
-                  <li class="user-footer">
-                    <div class="pull-left">
-                      <a href="#" class="btn btn-default btn-flat">Perfil</a>
-                    </div>
-                    <div class="pull-right">
-                      <a href="layouts/login" class="btn btn-default btn-flat">Desloguear</a>
-                    </div>
-                  </li>
-                </ul>
-              </li>
               <!-- Control Sidebar Toggle Button -->
               <li>
                 <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
